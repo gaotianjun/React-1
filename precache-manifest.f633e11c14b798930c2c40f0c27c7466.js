b510d5519eca599bf781fa37bf50be8291f90824
gaotianjun <44943100+gaotianjun@users.noreply.github.com>
2020-10-16 13:45:50 +0800

    Add files via upload
    react

---
+++ precache-manifest.f633e11c14b798930c2c40f0c27c7466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea440034a691e17a1e684229e1cf282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0ba27d0def31d976bc",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "d271315a8fce351da3ee",
+    "url": "/static/css/main.86edde70.chunk.css"
+  },
+  {
+    "revision": "4b0ba27d0def31d976bc",
+    "url": "/static/js/2.a8094ba1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8094ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d271315a8fce351da3ee",
+    "url": "/static/js/main.35f7446b.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
